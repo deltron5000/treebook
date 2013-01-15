c8f373b06ec3c6c79f416c8be0b3e286cc351523
Barry Delahunt <barrydelahunt@googlemail.com>
2013-01-15 15:38:54 +0000

    Created arails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '9d7681a0d61ba356558e1999553bb689b3fda7990938bb262fe62bdca15a1348e2b5e417851353ec40b100198b9be6b64ed6341207a589849b9a3be854fe275a'
